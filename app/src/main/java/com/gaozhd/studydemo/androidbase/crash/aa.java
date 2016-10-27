90501c9422e13886432bdd12d1dfbeee4857236f
MyGaozhd <gaozhidong@126.com>
2016-10-27 19:37:26 +0800

    1.捕获全局异常

---
+++ app/src/main/java/com/gaozhd/studydemo/androidbase/crash/aa.java
@@ -0,0 +1,10 @@
+package com.gaozhd.studydemo.androidbase.crash;
+
+/**
+ * @author Gao Zhidong <gaozhidong@tiantanhehe.com>
+ * @ClassName: aa
+ * @Description: ${todo}
+ * @date 2016/10/27 19:36
+ */
+public class aa {
+}
